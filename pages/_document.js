bc33f2c46d659e7e3a662c1da7e79b6f5e572a42
Challa Sharath <csharath@outlook.com>
2023-04-15 05:46:52 +0530

    push final changes

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+       <link rel="icon" href="/icon.svg" type="image/x-icon" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/icon.svg" />
+        <link rel="icon" type="image/svg" sizes="16x16" href="/icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
