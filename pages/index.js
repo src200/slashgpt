b72d87ea480288ff49ca9c8715e9666c10fb5170
Challa Sharath <csharath@outlook.com>
2023-04-15 06:18:41 +0530

    upload a demo video

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-        <p className="w-full justify-center border-yellow-50 pb-10">
-          <code className="font-bold text-3xl">/gpt</code>
-        </p>
-        <div className="flex w-full justify-center p-10 border-2 rounded-xl border-gray-600">
+    <main className="min-h-screen items-center p-24">
+        <div className="flex w-full justify-center flex-col p-10 items-center rounded-xl border-gray-600">
+           <p className="pb-10">
+            <code className="font-bold text-3xl">/gpt</code>
+          </p>
           <div>
-            Type /gpt + your request in any textbox and let ChatGPT do the job.
-            <br />
-            Get one lifetime license for $15. You will be redirected to the chrome extension immediately after the purchase.
+            <video class="bg-gray-100 rounded-lg shadow-lg" width="640" height="360" controls>
+              <source src="/demo.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
-          <div>
-
+          <div className="mt-10">
+            <a href="https://src200.gumroad.com/l/slashgpt" className="text-amber-500 cursor-pointer border uppercase border-amber-500 hover:bg-amber-500 hover:text-white active:bg-amber-600 font-bold px-8 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+              Get the chrome extension
+            </a>
           </div>
         </div>
     </main>
