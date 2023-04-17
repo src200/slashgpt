995b04ee2f4da8453fe8947f55914e2d22291b36
Challa Sharath <csharath@outlook.com>
2023-04-17 15:14:36 +0530

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,15 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-       <link rel="icon" href="/icon.svg" type="image/x-icon" />
-        <link rel="icon" type="image/svg" sizes="32x32" href="/icon.svg" />
-        <link rel="icon" type="image/svg" sizes="16x16" href="/icon.svg" />
+        <title>gpt</title>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
       </Head>
       <body>
         <Main />
